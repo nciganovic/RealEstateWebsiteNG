3c9b6045fddf59fcfa8b5011a1bdb577725d1dcc
nciganovic <nciganovic99@gmail.com>
2022-08-29 08:25:47 +0200

    add home as defualt route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { FindpropertyComponent } from './findproperty/findproperty.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
+  {path:'',pathMatch:"full", redirectTo: "/home"},
+  {path:'home',component:HomeComponent},
   {path:'properties',component:FindpropertyComponent},
   {path:'author',component:AuthorComponent},
   {path:'not-found',component:NotfoundComponent},
